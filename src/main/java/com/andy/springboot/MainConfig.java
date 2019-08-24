2e4853239b7e5d1b4d96c6fabfbd6c7cc42fa462
lianhong <lianhong@mama.cn>
2019-08-24 21:42:38 +0800

    master springboot整合rabbitmq消费者端

---
+++ src/main/java/com/andy/springboot/MainConfig.java
@@ -0,0 +1,14 @@
+package com.andy.springboot;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author lianhong
+ * @description
+ * @date 2019/8/24 0024下午 9:07
+ */
+@Configuration
+@ComponentScan("com.andy.springboot.*")
+public class MainConfig {
+}
